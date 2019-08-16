576cb4a086e9a01e7d9084f98aefb072b9724562
Matthew Mackreth <MMackreth@spartaglobal.com>
2019-08-16 09:31:43 +0100

    Finished nginx cookbook

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -15,15 +15,35 @@ describe 'nginx::default' do
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
-  end
 
-  context 'When all attributes are default, on CentOS 7' do
-    # for a complete list of available platforms and versions see:
-    # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
-    platform 'centos', '7'
-
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+    it 'default apt update' do
+      expect(chef_run).to update_apt_update 'update_sources'
     end
+
+    it "should install nginx" do
+      expect(chef_run).to install_package 'nginx'
+    end
+
+    it "should enable nginx service" do
+      expect(chef_run).to enable_service "nginx"
+    end
+
+    it "should start nginx service" do
+      expect(chef_run).to start_service "nginx"
+    end
+
+    it 'should create proxy template' do
+      expect(chef_run).to create_template("/etc/nginx/sites-available/proxy.conf").with_variables(proxy_port: 3000)
+    end
+
+    it "should create a symlink of proxy.conf" do
+      expect(chef_run).to create_link('/etc/nginx/sites-enabled/proxy.conf').with_link_type(:symbolic)
+    end
+
+    it 'should delete the pre-existing symlink' do
+      expect(chef_run).to delete_link('/etc/nginx/sites-enabled/default')
+    end
+
   end
+
 end
--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -5,12 +5,21 @@
 
 unless os.windows?
   # This is an example test, replace with your own test.
-  describe user('root'), :skip do
+  describe user('root') do
     it { should exist }
   end
 end
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+describe port(80) do
+  it { should be_listening }
+end
+
+describe service "nginx" do
+  it { should be_installed }
+  it { should be_running }
+  it { should be_enabled }
+end
+
+describe http('http://localhost', enable_remote_worker: true) do
+  its('status'){ should cmp 502 }
 end
